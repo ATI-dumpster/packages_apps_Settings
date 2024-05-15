5059c2007e12ca1012c48da159d7c659b30ac7e6
janak <niraulajanak2019@gmail.com>
2024-05-15 02:51:58 +0000

    Settings: Revamp about phone

--- src/com/android/settings/deviceinfo/aboutphone/EverestInfoPreferenceController.java
+++ src/com/android/settings/deviceinfo/aboutphone/EverestInfoPreferenceController.java
@@ -17,23 +17,26 @@
 
 package com.android.settings.deviceinfo.aboutphone;
 
-import java.io.IOException;
 import android.content.Context;
 import android.os.SystemProperties;
 import android.widget.TextView;
-import androidx.preference.PreferenceFragmentCompat;
+import android.text.TextUtils;
+
+import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
 import com.android.settings.utils.EverestSpecUtils;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.widget.LayoutPreference;
-import com.android.settingslib.Utils;
-import com.android.settings.core.PreferenceControllerMixin;
 
 public class EverestInfoPreferenceController extends AbstractPreferenceController {
 
     private static final String KEY_EVEREST_INFO = "everest_info";
+    private static final String KEY_STORAGE = "storage";
+    private static final String KEY_CHIPSET = "chipset";
+    private static final String KEY_BATTERY = "battery";
+    private static final String KEY_DISPLAY = "display";
 
     public EverestInfoPreferenceController(Context context) {
         super(context);
@@ -43,14 +46,22 @@ public class EverestInfoPreferenceController extends AbstractPreferenceControlle
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         final LayoutPreference everestInfoPreference = screen.findPreference(KEY_EVEREST_INFO);
-        final TextView processor = (TextView) everestInfoPreference.findViewById(R.id.processor_message);
-        final TextView storage = (TextView) everestInfoPreference.findViewById(R.id.storage_code_message);
-        final TextView battery = (TextView) everestInfoPreference.findViewById(R.id.battery_type_message);
-        final TextView infoScreen = (TextView) everestInfoPreference.findViewById(R.id.screen_message);
-        processor.setText(EverestSpecUtils.getProcessorModel());
-        storage.setText(String.valueOf(EverestSpecUtils.getTotalInternalMemorySize()) + "GB ROM | " + EverestSpecUtils.getTotalRAM() + " RAM");
-        battery.setText(EverestSpecUtils.getBatteryCapacity(mContext) + " mAh");
-        infoScreen.setText(EverestSpecUtils.getScreenRes(mContext));
+
+        if (everestInfoPreference != null) {
+            final TextView processor = everestInfoPreference.findViewById(R.id.chipset_summary);
+            final TextView storageAndRAM = everestInfoPreference.findViewById(R.id.cust_storage_summary);
+            final TextView battery = everestInfoPreference.findViewById(R.id.cust_battery_summary);
+            final TextView infoScreen = everestInfoPreference.findViewById(R.id.cust_display_summary);
+
+            if (processor != null && storageAndRAM != null && battery != null && infoScreen != null) {
+            Context context = everestInfoPreference.getContext();
+
+            processor.setText(EverestSpecUtils.getProcessorModel(context));
+            storageAndRAM.setText(EverestSpecUtils.getStorageAndRAMInfo(context));
+            battery.setText(EverestSpecUtils.getBatteryInfo(context));
+            infoScreen.setText(EverestSpecUtils.getScreenRes(context));
+            }
+        }
     }
 
     @Override
